aa9db53f36a3f7f37c657734212117806deb6867
Dominik Dlugajczyk <msocek@gmail.com>
2024-09-02 20:37:29 +0200

    Auth

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,25 @@
 <script setup>
+  import { onMounted, ref, computed } from 'vue'
+  import { supabase } from './models/supabase'
+
+  import { useStore } from 'vuex'
+  const store = useStore()
+
+  const isAuthenticated = computed(() => {
+    return store.getters["auth/isAuthenticated"]
+  })
+
+  onMounted(async () => {
+    await store.dispatch("auth/init")
+  })
 </script>
 
 <template>
   <div class="container" style="padding: 50px 0 100px 0">
     <nav>
       <div><RouterLink to="/">Home</RouterLink></div>
+      <div v-if="!isAuthenticated"><RouterLink to="/login">Login</RouterLink></div>
+      <div v-if="isAuthenticated"><RouterLink to="/account">Account</RouterLink></div>
       <div><RouterLink to="/editor">Panel Edytora</RouterLink></div>
     </nav>
     <main>
