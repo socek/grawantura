f5ab22fa1626715c78934bb9da61dea107dcd85e
Dominik Dlugajczyk <msocek@gmail.com>
2024-09-04 15:10:45 +0200

    WIP

---
+++ grawantura/src/pages/admin/dashboard/Dashboard.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import ProjectTable from './cards/ProjectTable.vue'
+import Timeline from './cards/Timeline.vue'
+</script>
+
+<template>
+  <h1 class="page-title font-bold">Dashboard</h1>
+  <section class="flex flex-col gap-4">
+    <div class="flex flex-col md:flex-row gap-4">
+      <ProjectTable class="w-full md:w-1/2" />
+    </div>
+  </section>
+</template>
